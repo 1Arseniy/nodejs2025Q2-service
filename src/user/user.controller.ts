87ad3ecffa083240f44ea7c098e07f6b056645f7
Arseniy <krutoi.arseniy@gmail.com>
2025-11-27 23:55:16 +0300

    feat: add user edpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/types/types';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -12,7 +26,29 @@ export class UserController {
   }
 
   @Get(':id')
-  findById(@Param('id') id: string): User {
+  findById(@Param('id', new ParseUUIDPipe()) id: string): User {
     return this.userService.findById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  create(@Body() CreateUserDto: CreateUserDto) {
+    return this.userService.create(CreateUserDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe())
+    id: string,
+    @Body()
+    UpdatePasswordDto: UpdatePasswordDto,
+  ) {
+    return this.userService.update(UpdatePasswordDto, id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { randomUUID } from 'crypto';
-import { Users } from 'src/types/types';
+import { User, Users } from 'src/types/types';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -10,7 +16,7 @@ export class UserService {
       id: randomUUID(),
       login: 'user',
       password: '1',
-      version: 1,
+      version: 0,
       createdAt: 2,
       updatedAt: 0,
     },
@@ -28,5 +34,40 @@ export class UserService {
     }
 
     return user;
+  }
+
+  create(dto: CreateUserDto) {
+    const date = Date.now();
+
+    const user: User = {
+      id: randomUUID(),
+      login: dto.login,
+      password: dto.password,
+      version: 0,
+      createdAt: date,
+      updatedAt: date,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  update(dto: UpdatePasswordDto, id: string) {
+    const date = Date.now();
+    const user = this.findById(id);
+
+    if (user.password !== dto.oldPassword) {
+      throw new ForbiddenException('Old Password is not correct');
+    }
+
+    (user.password = dto.newPassword), (user.updatedAt = date);
+    return user;
+  }
+
+  delete(id: string) {
+    const userForDel = this.findById(id);
+
+    this.users = this.users.filter((user) => user.id !== userForDel.id);
   }
 }
