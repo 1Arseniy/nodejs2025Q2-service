c5255a5b80260e5c7bcb98c5cc090cada108607f
Arseniy <krutoi.arseniy@gmail.com>
2025-12-05 21:50:02 +0300

    feat: add Dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavsModule } from './favs/favs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ArtistModule, AlbumModule, TrackModule, FavsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'my_database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,11 +1,23 @@
 import { randomUUID } from 'crypto';
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   login: string;
+
+  @Column()
   password: string;
+
+  @Column()
   version: number;
+
+  @Column()
   createdAt: number;
+
+  @Column()
   updatedAt: number;
 
   constructor(login: string, password: string) {
